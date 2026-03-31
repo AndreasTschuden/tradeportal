999dd65b4eab4551c400460d2c765db1884851be
tschudea07 <tschudea@edu.htl-villach.at>
2026-03-31 14:21:54 +0200

    route /home/products

---
+++ apps/web/src/app/(main)/home/products/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+
+export const metadata: Metadata = {
+  title: "TradePortal | Product Detail",
+  description: "Detailed information, reviews, and recommendations for a single product.",
+};
+
+export default function ProductDetailLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="py-2 md:px-30 px-5">
+      {children}
+    </div>
+  );
+}
