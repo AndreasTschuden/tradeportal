e3308ee2b8c9745f0135938330f92c4fb2ff268d
schauby1 <schauby@edu.htl-villach.at>
2026-04-05 16:42:43 +0200

    put passwords into env & started server deployment setup

--- apps/web/src/lib/minio.ts
+++ apps/web/src/lib/minio.ts
@@ -2,7 +2,7 @@ import * as Minio from 'minio'
 
 export const minioCompanyClient = new Minio.Client({ //read / write permissions
   endPoint: 'minio.tschudea.de',
-  port: 443,
+  port: 8999,
   useSSL: true,
   accessKey: process.env.MINIO_BUCKET_COMPANY_ACCESS_KEY,
   secretKey: process.env.MINIO_BUCKET_COMPANY_SECRET_KEY,
@@ -10,7 +10,7 @@ export const minioCompanyClient = new Minio.Client({ //read / write permissions
 
 export const minioCustomerClient = new Minio.Client({ //read permissions
   endPoint: 'minio.tschudea.de',
-  port: 443,
+  port: 8999,
   useSSL: true,
   accessKey: process.env.MINIO_BUCKET_CUSTOMER_ACCESS_KEY,
   secretKey: process.env.MINIO_BUCKET_CUSTOMER_SECRET_KEY,
